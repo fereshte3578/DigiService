897ffcf4b163644d9e275a9b964b99c7dca37dbd
fatemeh <fatemeh.mozaffari.77@gmail.com>
2020-08-04 12:40:25 +0430

    add profilemanager

---
+++ DigiService.BLL/userprofile/UserProfileManager.cs
@@ -0,0 +1,26 @@
+﻿using DigiService.DAL.userprofiledao;
+using DigiService.Entities.userprofileentities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiService.BLL.userprofile
+{
+    public class UserProfileManager
+    {
+        public bool Update_prof(UserProfileEntities userprof)
+        {
+            UserProfileDao userProfileDao = new UserProfileDao();
+            return userProfileDao.Update_prof(userprof);
+
+        }
+
+        public Entities.userprofileentities.UserProfileEntities selectuser(int id)
+        {
+            UserProfileDao userProfileDao = new UserProfileDao();
+            return userProfileDao.selectuser(id);
+        }
+    }
+}
