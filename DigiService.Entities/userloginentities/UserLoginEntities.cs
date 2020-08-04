0bfa8f2a62f99a4094ef7603689014a06c5f100f
fatemeh <fatemeh.mozaffari.77@gmail.com>
2020-08-04 13:53:23 +0430

    add web page login and one refrens

---
+++ DigiService.Entities/userloginentities/UserLoginEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiService.Entities.userloginentities
+{
+    public class UserLoginEntities
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DigiService.UIL/Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using DigiService.BLL.Userloginmanager;
+using DigiService.DAL.userlogindao;
+using DigiService.Entities.userloginentities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DigiService.UIL
+{
+    public partial class Login : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btn1_click(object sender, EventArgs e)
+        {
+
+            UserLoginManager userLoginManager = new UserLoginManager();
+            UserLoginEntities user = userLoginManager.loginuser(txtuname.Text, txtpassword.Text);
+            hfUserID.Value = user.id.ToString();
+            namehide.Value = user.Name.ToString();
+            familyhide.Value = user.Family.ToString();
+            phonehide.Value = user.Phone.ToString();
+            emailhise.Value = user.Email.ToString();
+            addresshide.Value = user.Address.ToString();
+            genderhide.Value = user.Gender.ToString();
+            Response.Cookies["Cookies3"]["id"] = hfUserID.Value;
+            Response.Cookies["Cookies3"]["name"] = namehide.Value;
+            Response.Cookies["Cookies3"]["family"] = familyhide.Value;
+            Response.Cookies["Cookies3"]["phone"] = phonehide.Value;
+            Response.Cookies["Cookies3"]["email"] = emailhise.Value;
+            Response.Cookies["Cookies3"]["address"] = addresshide.Value;
+            Response.Cookies["Cookies3"]["gender"] = genderhide.Value;
+            Response.Cookies["Cookies3"]["username"] = txtuname.Text;
+            Response.Cookies["Cookies3"]["password"] = txtpassword.Text;
+
+            Response.Cookies["StudentCookies"].Expires = DateTime.Now.AddDays(1);
+
+            Response.Redirect("Profile.aspx");
+        }
+    }
+}
