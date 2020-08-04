3dbbabbbaf1bb159ef3f1d1efbc1ba05a1282d39
fatemeh <fatemeh.mozaffari.77@gmail.com>
2020-08-04 13:27:17 +0430

    add class userdao and userentites to project

---
+++ DigiService.Entities/users/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiService.Entities.users
+{
+    public class Users
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
