32ef0cdf5a827fa22c3d70b1ced5375b03289c1b
fatemeh <fatemeh.mozaffari.77@gmail.com>
2020-08-04 13:22:24 +0430

    add page usermanager and one usercontrol

---
+++ DigiService.BLL/usermanager/UserManagers.cs
@@ -0,0 +1,24 @@
+﻿using DigiService.DAL.userdao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DigiService.BLL.usermanager
+{
+    public class UserManagers
+    {
+
+        public void AddData(string Name, string Family, string Phone, string Email, string Address, string Gender, string Username, string Password)
+        {
+            UserDao ob = new UserDao();
+            string Q = "INSERT INTO UserRegistration VALUES ('" + Name + "','" + Family + "','" + Phone + "','" + Email + "','" + Address + "','" + Gender + "','" + Username + "','" + Password + "') ";
+            ob.saveData(Q);
+        }
+    }
+
+}
+
---
+++ DigiService.UIL/UserControls/UserControlsignup_header.ascx
@@ -0,0 +1,26 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserControlsignup_header.ascx.cs" Inherits="DigiService.UIL.UserControls.UserControlsignup_header" %>
+<body dir="rtl">
+
+    <div class="topnav">
+        <div class="row ">
+            <div class="col-lg-4  col-sm-12" dir="rtl">
+                <img class="img1" src="Images/erpLogo.png"  />
+            </div>
+            <div class="col-lg-4 col-sm-12">
+              
+            </div>
+
+
+            <div class="col-lg-4 col-sm-12" style="float: left; padding-left: 2%; margin-top: 10px">
+
+               
+              
+
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
