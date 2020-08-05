b853078ded6cf9a0aad01b0d5ad519444bec8bae
Fereshte Moazzeni <62180046+fereshte3578@users.noreply.github.com>
2020-08-05 18:34:21 +0430

    change conected

--- DigiService.DAL/HeadNameResourceDao.cs
+++ DigiService.DAL/HeadNameResourceDao.cs
@@ -18,9 +18,8 @@ namespace DigiService.DAL
             //1.1.make new connection
 
 
-            SqlConnection sqlConnection = new SqlConnection("Data Source=.;Initial Catalog = DigiService;Integrated Security = true");
+            SqlConnection sqlConnection = new SqlConnection("Data Source=.;Initial Catalog = DigiService;Integrated Security = False ;User ID = Digiservice ; Password = fereshte7835; ") ;
             //make a command
-
 
             SqlCommand sqlCommand = sqlConnection.CreateCommand();
             sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
